f265c63d432242c8e2817abddabeef0db5a747fa
Andres Gomez <98500138+TheAndresG@users.noreply.github.com>
2022-08-16 22:46:35 -0300

    Update Menu-App.js

--- components/Menu-App.js
+++ components/Menu-App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Productos from "./Productos/Productos";
 
 export default function Prueba() {
   return (
     <View style={styles.container}>
       <Text>Hello world2ssss</Text>
-      <Productos />
       <StatusBar style="auto" />
     </View>
   );
