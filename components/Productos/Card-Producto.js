222d394146e6e37f0097259746a6dd39830f3179
Andres Gomez Guardamagna <andresgomezguardamagna@gmail.com>
2022-08-17 22:28:35 -0300

    CSS productos y cards

--- components/Productos/Card-Producto.js
+++ components/Productos/Card-Producto.js
@@ -4,21 +4,28 @@ import { StyleSheet, Text, View } from "react-native";
 const CardProducto = ({ nombre, categoria, precio }) => {
   return (
     <View style={estilo.lista}>
-      <Text style={estilo.texto}>{nombre}</Text>
-      <Text style={estilo.texto}>{categoria}</Text>
-      <Text style={estilo.texto}>{precio} </Text>
+      <Text style={estilo.texto1}>{nombre}</Text>
+      <Text style={estilo.texto2}>{categoria}</Text>
+      <Text style={estilo.texto3}>{precio} </Text>
     </View>
   );
 };
 
 let estilo = StyleSheet.create({
   lista: {
+    backgroundColor: "#F8E9E9",
+    margin: 5,
     displey: "flex",
     flexDirection: "row",
-    width: "60%",
+    width: "95%",
+    height: 40,
+    alignItems: "center",
+    padding: 10,
     justifyContent: "space-between",
   },
-  texto: { color: "black" },
+  texto1: { color: "black", width: "30%" },
+  texto2: { color: "black", textAlign: "center", width: "30%" },
+  texto3: { color: "black", textAlign: "right", width: "30%" },
 });
 
 export default CardProducto;
--- components/Productos/Productos.js
+++ components/Productos/Productos.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import CardProducto from "./Card-Producto";
 
 const Productos = () => {
@@ -7,21 +7,42 @@ const Productos = () => {
     { id: 1, nombre: "Queso", categoria: "Lacteo", precio: "600" },
     { id: 2, nombre: "Pan", categoria: "Panaderia", precio: "350" },
     { id: 3, nombre: "Fiambre", categoria: "Fiambreria", precio: "400" },
+    { id: 4, nombre: "Queso", categoria: "Lacteo", precio: "600" },
+    { id: 5, nombre: "Pan", categoria: "Panaderia", precio: "350" },
+    { id: 6, nombre: "Fiambre", categoria: "Fiambreria", precio: "400" },
+    { id: 7, nombre: "Queso", categoria: "Lacteo", precio: "600" },
+    { id: 8, nombre: "Pan", categoria: "Panaderia", precio: "350" },
+    { id: 9, nombre: "Fiambre", categoria: "Fiambreria", precio: "400" },
+    { id: 10, nombre: "Queso", categoria: "Lacteo", precio: "600" },
+    { id: 11, nombre: "Queso", categoria: "Lacteo", precio: "600" },
+    { id: 12, nombre: "Pan", categoria: "Panaderia", precio: "350" },
+    { id: 13, nombre: "Fiambre", categoria: "Fiambreria", precio: "400" },
+    { id: 14, nombre: "Queso", categoria: "Lacteo", precio: "600" },
+    { id: 15, nombre: "Pan", categoria: "Panaderia", precio: "350" },
+    { id: 16, nombre: "Fiambre", categoria: "Fiambreria", precio: "400" },
   ];
   return (
-    <View>
-      {numero.map((e) => {
-        return (
-          <CardProducto
-            key={e.id}
-            nombre={e.nombre}
-            categoria={e.categoria}
-            precio={e.precio}
-          />
-        );
-      })}
+    <View style={estilos.lista}>
+      <FlatList
+        data={numero}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <CardProducto
+              key={item.id}
+              nombre={item.nombre}
+              categoria={item.categoria}
+              precio={item.precio}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
+
+const estilos = StyleSheet.create({
+  lista: { alignItems: "center" },
+});
 
 export default Productos;
