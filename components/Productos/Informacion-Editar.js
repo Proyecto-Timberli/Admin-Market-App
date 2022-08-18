777f0e4bd38dd59d5f6c9417c025221a4dde0950
LucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2022-08-18 19:58:25 -0300

    pasaron cosas

---
+++ components/Productos/Informacion-Editar.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { TextInput, Pressable, Button, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function Informacion() {
+    
+  return (
+    <View>
+        
+    </View>
+  )
+}
